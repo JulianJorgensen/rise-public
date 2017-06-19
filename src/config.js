9845989cdedc8d85270628f8ac2e4e4955c3a05a
Julian Jorgensen <me@julianjorgensen.com>
2017-06-19 09:57:09 +0800

    fixing profileParamsToPopulate

--- src/config.js
+++ src/config.js
@@ -9,15 +9,12 @@ export const firebase = {
 // For more details, visit https://prescottprue.gitbooks.io/react-redux-firebase/content/config.html
 export const reduxFirebase = {
   userProfile: 'users', // root that user profiles are written to
-  profileParamsToPopulate: [
-    { child: 'role', root: 'roles' }, // populates user's role with matching role object from roles
-  ],
+  profileParamsToPopulate: ['role:roles'],
   // enableLogging: false, // enable/disable Firebase Database Logging
-  // updateProfileOnLogin: false, // enable/disable updating of profile on login
+  // updateProfileOnLogin: true, // enable/disable updating of profile on login
   profileFactory: (user) => {
     return ({
       email: user.email || user.providerData[0].email,
-      roles: 'athlete',
       showLeftNavigation: true
     })
   }
