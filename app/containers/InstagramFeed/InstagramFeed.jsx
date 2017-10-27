08a8a965dda50ff0be4a371d2ef14b807e4dad99
Julian Jorgensen <me@julianjorgensen.com>
2017-10-27 10:40:04 +0800

    Fix instagram feed

--- app/containers/InstagramFeed/InstagramFeed.jsx
+++ app/containers/InstagramFeed/InstagramFeed.jsx
@@ -14,7 +14,7 @@ export default class InstagramFeed extends React.Component {
             template={`<div class=${styles.item} style="background-image: url({{image}});"><a class=${styles.link} href={{link}} target="_new"></a></div>`}
             userId='2234856285'
             clientId='023dfe5661274e1bae8b1a0ec3dc7f1d'
-            accessToken='2234856285.023dfe5.12c5c36e79f14f1cab36233c8856324e' // 2ca02d85b5aa48fdb97158e75cf5af78
+            accessToken='2234856285.023dfe5.c8a202b23ddb409db9e1f4918156440a' // 2ca02d85b5aa48fdb97158e75cf5af78
           />
         </div>
     )
